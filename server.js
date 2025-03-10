1e634dd43057a216aa7ab227f5f6c74be83e342e
hussain-wac <hussain.n@webandcrafts.in>
2025-03-10 16:39:51 +0530

    appolo

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 // Sample data
-const books = [
-  { title: '1984', author: 'George Orwell' },
-  { title: 'Brave New World', author: 'Aldous Huxley' },
-];
+const books = [];
 
-// Define the GraphQL schema (typeDefs)
 const typeDefs = `#graphql
   type Book {
     title: String
@@ -17,24 +13,30 @@ const typeDefs = `#graphql
   type Query {
     books: [Book]
   }
+
+  type Mutation {
+    createBook(title: String!, author: String!): Book
+  }
 `;
 
-// Define resolvers (functions to fetch the data)
 const resolvers = {
   Query: {
     books: () => books,
   },
+  Mutation: {
+    createBook: (_, { title, author }) => {
+      const newBook = { title, author };
+      books.push(newBook);
+      return newBook;
+    },
+  },
 };
 
-// Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Start the Apollo Server
-startStandaloneServer(server, {
-  listen: { port: 5000 },  // The server will run on http://localhost:4000
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+startStandaloneServer(server, { listen: { port: 5000 } }).then(({ url }) => {
+  console.log(`Server running at ${url}`);
 });
